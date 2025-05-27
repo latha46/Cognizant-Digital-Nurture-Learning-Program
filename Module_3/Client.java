1da4df4b9b64f3231be962e982329639a2800eaf
latha46 <lathachangade1497@gmail.com>
2025-05-27 14:22:48 +0530

    Create Client.java

---
+++ Module_3/Client.java
@@ -0,0 +1,19 @@
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 5000);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        String userInput;
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+            System.out.println("Server says: " + in.readLine());
+        }
+
+        socket.close();
+    }
+}
