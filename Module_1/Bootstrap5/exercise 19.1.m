8083d826e09476683270e7849102724790997386
latha46 <lathachangade1497@gmail.com>
2025-05-27 10:59:47 +0530

    Create exercise 19.1.m

---
+++ Module_1/Bootstrap5/exercise 19.1.m
@@ -0,0 +1,12 @@
+
+// Import Bootstrap functions and variables
+@import "node_modules/bootstrap/scss/functions";
+@import "node_modules/bootstrap/scss/variables";
+@import "node_modules/bootstrap/scss/mixins";
+
+// You can now override Bootstrap variables here BEFORE importing rest of Bootstrap
+// For example:
+// $primary: #ff6600;
+// $border-radius: 1rem;
+
+@import "node_modules/bootstrap/scss/bootstrap";
