2c956298fb012d1d8da3b0254c1e4e2426a6cf78
latha46 <lathachangade1497@gmail.com>
2025-05-27 14:38:04 +0530

    Create TryCatch.java

---
+++ Module_3/TryCatch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter numerator (integer): ");
+        int numerator = sc.nextInt();
+
+        System.out.print("Enter denominator (integer): ");
+        int denominator = sc.nextInt();
+
+        try {
+            int result = numerator / denominator;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+    }
+}
